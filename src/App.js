320b46612ca483d80a79a550066f5f1f9f86a7c3
Usman-tpr <usman.tpr@gmail.com>
2022-11-02 04:44:35 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import About from "./components/About";
+import Sidebar from "./components/Sidebar";
+function App() {
+  const [toggle, setToggle] = React.useState(false);
+  console.log(toggle);
+  return (
+    <main>
+      <Navbar toggle={setToggle} />
+      <Sidebar toggle={toggle} setToggle={setToggle} />
+      <Header />
+      <About />
+   
+   
+    </main>
+  );
+}
+
+export default App;
